f08831f17b02259db3990d8f8aff94c66f009efe
alettam <alettam14@gmail.com>
2020-06-23 16:04:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/loginwithcheckbox/MainActivity.java
@@ -0,0 +1,204 @@
+package com.example.loginwithcheckbox;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private Button registrationBtn;
+    private CheckBox checkBox;
+    private EditText loginEdTxt;
+    private EditText passwordEdTxt;
+
+    private static String STORAGE_FILE = "storage_file.txt";
+    private File file;
+    private SharedPreferences saveTxt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        initViews();
+    }
+
+    private void initViews() {
+        loginEdTxt = findViewById(R.id.loginEdTxt);
+        passwordEdTxt = findViewById(R.id.passwordEdTxt);
+        checkBox = findViewById(R.id.checkBox);
+        saveTxt = getSharedPreferences("PREFS_NAME", 0);
+        checkBox.setChecked(saveTxt.getBoolean("isChecked", false));
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = saveTxt.edit();
+                editor.putBoolean("isChecked", isChecked);
+                editor.apply();
+            }
+        });
+
+
+        registrationBtn = findViewById(R.id.regBtn);
+        registrationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkData()) {
+                    registrationIn();
+                    Toast.makeText(MainActivity.this, "Регистрация прошла успешно", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Введите данные", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkData()) {
+                    singIn();
+                } else {
+                    Toast.makeText(MainActivity.this, "Введите данные", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    private void registrationIn() {
+        if (checkBox.isChecked()) {
+            saveExternalFile(loginEdTxt.getText().toString(), passwordEdTxt.getText().toString());
+        } else {
+            saveFile("login", loginEdTxt.getText().toString(), passwordEdTxt.getText().toString());
+        }
+    }
+
+    private void saveExternalFile(String login, String password) {
+        file = new File(getExternalFilesDir(null), STORAGE_FILE);
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(file));
+            writer.write(login + ";" + password);
+            writer.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void singIn() {
+        if (checkBox.isChecked()) {
+            if(readExternalFile(loginEdTxt.getText().toString(), passwordEdTxt.getText().toString())){
+                Toast.makeText(MainActivity.this, "Вход выполнен", Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(MainActivity.this, "Введены неправильный логин или пароль", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            if (readFile("login", loginEdTxt.getText().toString(), passwordEdTxt.getText().toString())) {
+                Toast.makeText(MainActivity.this, "Вход выполнен", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Введены неправильный логин или пароль", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    private boolean readExternalFile(String login, String password) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            try {
+                String line = reader.readLine();
+                while (line != null) {
+                    String [] data = line.split(";");
+                    Log.d("Tag", line);
+                    return (data[0].equals(login) && data[1].equals(password));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean readFile(String fileName, String login, String password) {
+        try {
+            FileInputStream fileInputStream = openFileInput(fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            try {
+                String line = reader.readLine();
+                while (line != null) {
+                    String [] data = line.split(";");
+                    Log.d("Tag", line);
+                    return (data[0].equals(login) && data[1].equals(password));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+
+    private void saveFile(String fileName, String login, String password) {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            BufferedWriter bw = new BufferedWriter(outputStreamWriter);
+            try {
+                bw.write(login + ";" + password);
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private boolean checkData() {
+        return !TextUtils.isEmpty(loginEdTxt.getText().toString()) && !TextUtils.isEmpty(passwordEdTxt.getText().toString());
+    }
+}
